05717c954a43f7a07b24636a6df56bc56b67d3e8
Sarvesh <>
2026-04-04 21:34:34 +0530

    added vercel.json

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
 const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:9000/api";
+  `${import.meta.env.VITE_API_BASE_URL}/api` || "http://localhost:9000/api";
 
 export const API_PATHS = {
   AUTH: {
