48be7d1b95c35b2b4cd38d45cabb3f0343433ca9
Sarvesh <>
2026-04-05 03:28:04 +0530

    login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="h-screen flex items-center justify-center px-4">
+      <div className="w-full max-wd-md bg-white p-8 rounded-2xl shadow-lg">
+        <h2 className="text-2xl font-bold text-center mb-2">
+          Welcome Back
+        </h2>
 
-export default Login
+        <p>Login to continue your interview prepration</p>
+
+        <input type="email" name="email" id="email" placeholder="Enter Email" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Enter Password"
+        />
+        <button className="w-full bg-black text-white py-3">
+          Login
+        </button>
+
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
